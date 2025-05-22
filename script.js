cd8c0fe84272e0433614df0c976fdc35f0e2d286
John Kwong <jkfc0212@gmail.com>
2025-05-22 15:43:41 -0400

    init

--- script.js
+++ script.js
@@ -340,7 +340,19 @@ document.addEventListener('DOMContentLoaded',  function() {
       addCustomTag(inputText.value.trim());
       inputText.value = '';
     }
+
+function onScanSuccess(decodedText, decodedResult) {
+    // Handle on success condition with the decoded text or result.
+    console.log(`Scan result: ${decodedText}`, decodedResult);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
+
+
   });
+
 
   // Add event listener for the API form
   const apiForm = document.getElementById('apiForm');
